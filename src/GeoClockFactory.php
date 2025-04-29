feefc4bd4fe6a5053f1acf9aa554fcb60565e247
Oleg Shevchuk <ted7021@gmail.com>
2025-04-29 08:11:48 +0300

    Add GeoClockFactory class

--- src/GeoClockFactory.php
+++ src/GeoClockFactory.php
@@ -1,10 +1,29 @@
 <?php
-declare(strict_types=1);
-
 
 namespace GeoClock;
 
+use GeoClock\Provider\TimeApiProvider;
+use GeoClock\Provider\WorldTimeApiProvider;
+use GeoClock\IpResolver\IpifyResolver;
+use Psr\Clock\ClockInterface;
+
 class GeoClockFactory
 {
+    public static function createWithProviders(array $providers, ?string $ip = null): ClockInterface
+    {
+        $config = new Config($providers);
+        return new Clock($config, $ip);
+    }
 
+    public static function create(?string $ip = null): ClockInterface
+    {
+        $ipResolver = new IpifyResolver();
+
+        $providers = [
+            new TimeApiProvider($ipResolver),
+            new WorldTimeApiProvider(),
+        ];
+
+        return self::createWithProviders($providers, $ip);
+    }
 }
--- tests/GeoClockFactoryTest.php
+++ tests/GeoClockFactoryTest.php
@@ -2,17 +2,21 @@
 
 namespace GeoClock\Tests;
 
+use GeoClock\GeoClockFactory;
+use Psr\Clock\ClockInterface;
 use PHPUnit\Framework\TestCase;
 
 class GeoClockFactoryTest extends TestCase
 {
     public function testCanCreateClockWithDefaultProviders(): void
     {
-        $this->markTestIncomplete('Not implemented yet.');
+        $clock = GeoClockFactory::create();
+        $this->assertInstanceOf(ClockInterface::class, $clock);
     }
 
     public function testCanCreateClockWithIpGeolocationProvider(): void
     {
-        $this->markTestIncomplete('Not implemented yet.');
+        $clock = GeoClockFactory::create(null, 'fake-api-key');
+        $this->assertInstanceOf(ClockInterface::class, $clock);
     }
 }
